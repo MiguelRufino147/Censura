8948cb1e1ced9f11e3bd87ab1d13489c4519acaa
Miguel Rufino <miguelrcostaalves@hotmail.com>
2022-11-07 23:37:17 -0300

    Censura

--- Change.py
+++ Change.py
@@ -1,47 +1,58 @@
-#Processo para pegar o e-mail#
 
-email=input('Insira o E-mail desejado').strip().split(" ")
-if email =='':
-    print('Nenhum email inserido')
-
-#lista palavras#
-palavras=input('Palavras Restritas').strip().split(" ") #Coloque todas palavras a serem restrita
+def principal():                    #FUNÇÃO PRINCIPAL
+    submit=0
+    frase = input('Seu email').strip().lower()
+    proibidas = []
 
 
-for p in palavras:
-    nmrpalavras=len(p)    #Usado para ver quantas letras possui a palavra restrita
+    while submit == 0:
+        if frase == '':
+            inputEmail = input('Digite um email: ')                 #CRIANDO LOOP
+            frase = inputEmail
+
+        print('')
+        print('Digite "comandos" para mostrar os comandos disponiveis.')
+        print('')
+
+        command = input('=> ')
+
+        if command == 'comandos':
+            print('')
+            print('add - Adiciona uma nova palavra para censurar.')
+            print('submit - Executa a censura das palavras selecionadas.')
+            print('')
+
+        if command == 'add':
+            word = input('|_ Digite uma nova palavra/frase: ')
+            proibidas.append(word)                                                  #LISTA IF'S DE OPÇÕES PARA SELECIONAR
+            for word in proibidas:
+                frase = frase.replace(word, "*" * len(word))
+                print('-' * 30)
+                print(proibidas)
+
+        if command == 'submit':
+            for word in proibidas:
+                frase = frase.replace(word, "*" * len(word))
+            print('TEXTO ORIGINAL:')
+            print(frase)
+            print(' ')
+            print('\033[1;30;41mPALAVRAS PROIBIDAS:\033[m')
+
+            print(proibidas)
+
+#chamando função
+principal()                    #CHAMANDO FUNÇÃO
 
 
-#localizar palavras específicas#
-
-def localizar(x):
-        if x in palavras:
-            emailsbs = x.replace(x, "*" * nmrpalavras)   #função para substituir palavras para *
-            return emailsbs
-
-        else:
-            return x
-
-def retornar(x):
-    if x in palavras:                                #Retorna palavras censuradas sem o *
-        return x
-    else:
-        return ''
 
 
-#texto final
 
-l=''
-l2=''
-for words in email:
-    l+=localizar(words)+' '
-    l2+=retornar(words)+ ' '
-print('Texto substituido:')                                   #Pega as palavras e joga nas funções
-print('-' * 30)
-print(l)
-print('-' * 30)
-print(f'\033[1;30;41mPALAVRAS RESTRITAS ENCONTRADAS:\033[m')
-print(l2)
+
+
+
+
+
+
 
 
 
